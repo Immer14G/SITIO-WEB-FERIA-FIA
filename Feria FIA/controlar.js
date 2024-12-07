34399c9bb05287ca8d7c9f56c4d6bcb2cc04ca18
Immer Nehemías Guevara Ramos <neheguevara14@gmail.com>
2024-12-07 09:35:52 -0600

    js funcional

--- Feria FIA/controlar.js
+++ Feria FIA/controlar.js
@@ -1,8 +1,19 @@
+function mostrarInformacion(titulo, descripcion, imagenSrc) {
+    const modal = document.getElementById("modal");
+    const modalTitulo = document.getElementById("modal-titulo");
+    const modalDescripcion = document.getElementById("modal-descripcion");
+    const modalImagen = document.getElementById("modal-imagen");
 
-function mostrarInformacion(titulo, descripcion) {
-    alert(`Título: ${titulo}\nDescripción: ${descripcion}`);
+    modalTitulo.textContent = titulo;
+    modalDescripcion.textContent = descripcion;
+    modalImagen.src = imagenSrc;
+
+    modal.style.display = "block";
 }
 
+function cerrarModal() {
+    document.getElementById("modal").style.display = "none";
+}
 
 function mostrarCarreras() {
     const carreras = [
