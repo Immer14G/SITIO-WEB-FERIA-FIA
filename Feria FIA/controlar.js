6050062757ae253892ae89f8a7d13e86af8a9223
Fer2345Romero <fernandaromeroclaros@gmail.com>
2024-12-08 17:46:41 -0600

    Agregando Dashboard con las carreras FIA y seccionando el sitio web

--- Feria FIA/controlar.js
+++ Feria FIA/controlar.js
@@ -14,14 +14,3 @@ function mostrarInformacion(titulo, descripcion, imagenSrc) {
 function cerrarModal() {
     document.getElementById("modal").style.display = "none";
 }
-
-function mostrarCarreras() {
-    const carreras = [
-        "Arquitectura",
-        "Ingeniería Civil",
-        "Ingeniería en Desarrollo de Software",
-        "Ingeniería Industrial",
-        "Ingeniería Eléctrica"
-    ];
-    alert("Carreras participantes:\n" + carreras.join("\n"));
-}
